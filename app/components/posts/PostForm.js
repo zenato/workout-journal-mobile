893eba5fc0fa8439d8fc2528125d7d89dd6b1a7c
Yeongjin Lee <yeongjinnn@gmail.com>
2018-01-16 16:47:23 +0900

    Remove unnecessary state.

--- app/components/posts/PostForm.js
+++ app/components/posts/PostForm.js
@@ -15,41 +15,31 @@ type Props = {
 }
 
 export default class PostForm extends Component<Props> {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      isWorkoutDateOpen: false,
-      workoutDate: _.get(props.item, 'workoutDate', new Date()),
-    }
+  state = {
+    isWorkoutDateOpen: false,
   }
 
   handlePressWorkoutDate = () => {
     this.setState(state => ({ isWorkoutDateOpen: !state.isWorkoutDateOpen }))
   }
 
-  handleWorkoutDateChange = workoutDate => {
-    this.setState({ workoutDate })
-    this.props.onChange('workoutDate', workoutDate)
-  }
-
   render() {
     const { isLoading, item, onChange } = this.props
-    const { isWorkoutDateOpen, workoutDate } = this.state
+    const { isWorkoutDateOpen } = this.state
     return (
       <View>
         <Field label="Date">
           <TouchableOpacity onPress={this.handlePressWorkoutDate}>
             <Text style={[styles.workoutDateText, isWorkoutDateOpen && styles.workoutDateActive]}>
-              {format(workoutDate, 'MM/DD/YYYY')}
+              {format(item.workoutDate, 'MM/DD/YYYY')}
             </Text>
           </TouchableOpacity>
         </Field>
         {isWorkoutDateOpen && (
           <DatePickerIOS
             mode="date"
-            date={workoutDate}
-            onDateChange={this.handleWorkoutDateChange}
+            date={item.workoutDate}
+            onDateChange={workoutDate => onChange('workoutDate', workoutDate)}
           />
         )}
         <Field label="Remark">
